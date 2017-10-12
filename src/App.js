d0c38029fb7fe7b05734fa483a93376634a2b0b7
huanghui <hunghoo@163.com>
2017-10-12 20:12:41 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import Container from "./container/index";
 class App extends Component {
 
   render() {
-    const {store} = this.props;
     return (
       <div className="App">
-        <Container {...store}/>   
       </div>
     );
   }
